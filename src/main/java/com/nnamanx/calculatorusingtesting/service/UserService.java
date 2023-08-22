6b3b9a90ee205712875d908b8945593647631647
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-22 17:56:22 +0400

    "User Service: save user method"

--- src/main/java/com/nnamanx/calculatorusingtesting/service/UserService.java
+++ src/main/java/com/nnamanx/calculatorusingtesting/service/UserService.java
@@ -1,7 +1,21 @@
 package com.nnamanx.calculatorusingtesting.service;
 
+import com.nnamanx.calculatorusingtesting.model.MyUser;
+import com.nnamanx.calculatorusingtesting.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public MyUser saveUser(MyUser myUser) {
+
+        myUser.setActive(true);
+        return userRepository.save(myUser);
+    }
+
+    
 }
