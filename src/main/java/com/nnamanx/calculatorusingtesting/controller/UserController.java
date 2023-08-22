a1007647605150282c49ac4bd51389cf0281848b
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-22 18:18:59 +0400

    "User Controller"

---
+++ src/main/java/com/nnamanx/calculatorusingtesting/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.nnamanx.calculatorusingtesting.controller;
+
+import com.nnamanx.calculatorusingtesting.model.MyUser;
+import com.nnamanx.calculatorusingtesting.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("testing/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @GetMapping("/{username}")
+    public ResponseEntity<MyUser> getUserByUsername(@PathVariable String username) {
+        
+        MyUser user = userService.getUserByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<MyUser> createUser(@RequestBody MyUser user) {
+
+        MyUser createdUser = userService.saveUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+
+}
--- src/test/java/com/nnamanx/calculatorusingtesting/service/UserServiceTesting.java
+++ src/test/java/com/nnamanx/calculatorusingtesting/service/UserServiceTesting.java
@@ -1,15 +1,45 @@
 package com.nnamanx.calculatorusingtesting.service;
 
-import org.mockito.InjectMocks;
+import com.nnamanx.calculatorusingtesting.model.MyUser;
+import com.nnamanx.calculatorusingtesting.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @SpringBootTest
 public class UserServiceTesting {
 
-
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public UserServiceTesting(UserService userService) {
+    public UserServiceTesting(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
+
+    @Test
+    public void TestGetUserByUsername() {
+
+        String username = "Laman";
+
+        MyUser user = new MyUser();
+        user.setName(username);
+
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+        MyUser dbUsername = userService.getUserByUsername(username);
+
+        assertNotNull("The user does not exist!", dbUsername); //checking whether the value is null
+
+        assertEquals("The message", username, dbUsername.getName());
+
+    }
+
+
 }
