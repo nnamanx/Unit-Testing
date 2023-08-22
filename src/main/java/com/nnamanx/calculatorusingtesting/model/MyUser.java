7138c03815d5fca15df76253f125e5c823014eb9
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-22 17:45:32 +0400

    "my User entity"

---
+++ src/main/java/com/nnamanx/calculatorusingtesting/model/MyUser.java
@@ -0,0 +1,27 @@
+package com.nnamanx.calculatorusingtesting.model;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+    String email;
+    boolean isActive;
+}
